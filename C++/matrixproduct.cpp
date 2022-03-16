a587c784f69a5583ded6c7f4f778a2eb5a426c17
Monkin <monkinsmurf@gmail.com>
2022-03-16 12:32:30 +0000

    Started matrix block multiplicatoin

--- C++/matrixproduct.cpp
+++ C++/matrixproduct.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <cstdlib>
 #include <papi.h>
+#include <cmath>
 
 using namespace std;
 
@@ -123,18 +124,73 @@ void OnMultLine(int m_ar, int m_br)
 }
 
 // add code here for block x block matriz multiplication
-void OnMultBlock(int m_ar, int m_br, int bkSize)
-{
+void OnMultBlock(int m_ar, int m_br, int bkSize) {
+    SYSTEMTIME Time1, Time2;
+
+    char st[100];
+    int i, j, k, i0, j0, k0;
+
+    double temp;
+    double *pha, *phb, *phc;
+
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+    for (i = 0; i < m_ar; i++)
+        for (j = 0; j < m_ar; j++)
+            pha[i * m_ar + j] = (double)1.0;
+
+    for (i = 0; i < m_br; i++)
+        for (j = 0; j < m_br; j++)
+            phb[i * m_br + j] = (double)(i + 1);
+
+    for (i = 0; i < m_ar; i++)
+        for (j = 0; j < m_ar; j++)
+            phc[i * m_ar + j] = (double)0;
+
+    Time1 = clock();
+
+     for (i0 = 0; i0 < m_ar; i0 += bkSize) {
+        for (j0 = 0; j0 < m_br; j0 += bkSize) {
+            for (k0 = 0; k0 < m_ar; k0 += bkSize) {
+                for (i = i0; i < min(i0+bkSize, m_ar); i++) {
+                    for (j = j0; j < min(j0+bkSize, m_br); j++) {
+                        temp = 0;
+                        for (k = k0; k < min(k0+bkSize, m_ar); k++) {
+                            temp += pha[i * m_ar + k] * phb[k * m_br + j];
+                        }
+                        phc[i * m_ar + j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    Time2 = clock();
+    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+    cout << st;
+
+    // display 10 elements of the result matrix tto verify correctness
+    cout << "Result matrix: " << endl;
+    for (i = 0; i < 1; i++)
+    {
+        for (j = 0; j < min(10, m_br); j++)
+            cout << phc[j] << " ";
+    }
+    cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
 }
 
-void handle_error(int retval)
-{
+void handle_error(int retval) {
     printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
     exit(1);
 }
 
-void init_papi()
-{
+void init_papi() {
     int retval = PAPI_library_init(PAPI_VER_CURRENT);
     if (retval != PAPI_VER_CURRENT && retval < 0)
     {
