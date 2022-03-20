28022123d9ecc9bb354821d716c73d9030f5661e
monkin <monkinsmurf@gmail.com>
2022-03-20 16:32:05 +0000

    Implemented block matrix

--- C++/matrixproduct.cpp
+++ C++/matrixproduct.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <cstdlib>
 #include <papi.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -125,6 +126,68 @@ void OnMultLine(int m_ar, int m_br)
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
+    SYSTEMTIME Time1, Time2;
+
+    char st[100];
+    double temp;
+    int i, j, k, ii, jj, kk;
+
+    double *pha, *phb, *phc;
+
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+    for (i = 0; i < m_ar; i++)
+        for (j = 0; j < m_ar; j++)
+            pha[i * m_ar + j] = (double)1.0;
+
+    for (i = 0; i < m_br; i++)
+        for (j = 0; j < m_br; j++)
+            phb[i * m_br + j] = (double)(i + 1);
+
+    for (i = 0; i < m_br; i++)
+        for (j = 0; j < m_br; j++)
+            phc[i * m_br + j] = (double)(0.0);
+
+    Time1 = clock();
+
+    for (ii = 0; ii < m_ar; ii += bkSize)
+    {
+        for (jj = 0; jj < m_br; jj += bkSize)
+        {
+            for (kk = 0; kk < m_ar; kk += bkSize)
+            {
+                for (i = ii; i < min(ii + bkSize, m_ar); i++)
+                {
+                    for (j = jj; j < min(jj + bkSize, m_br); j++)
+                    {
+                        for (k = kk; k < min(kk + bkSize, m_ar); k++)
+                        {
+                            phc[i * m_ar + j] += pha[i * m_ar + k] * phb[k * m_ar + j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Time2 = clock();
+    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+    cout << st;
+
+    // display 10 elements of the result matrix tto verify correctness
+    cout << "Result matrix: " << endl;
+    for (i = 0; i < 1; i++)
+    {
+        for (j = 0; j < min(10, m_br); j++)
+            cout << phc[j] << " ";
+    }
+    cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
 }
 
 void handle_error(int retval)
